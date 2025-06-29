5bfa639a15c723a2940a3806915098f973c7cca7
Satish Sonone <125435030+Satish9284@users.noreply.github.com>
2025-06-29 16:55:52 +0530

    Update helloworld.java

--- helloworld.java
+++ helloworld.java
@@ -1,5 +1,5 @@
 public class helloworld {
    public static void main(String[] args) {
-    System.out.println("Hello World - Feb 15th 2025 - 09:55 PM - triggered by webhook");
+    System.out.println("Hello World - Feb 15th 2024 - 09:24 PM - triggered by webhook");
     }
 }
